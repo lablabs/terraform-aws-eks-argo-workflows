60e62af25276340c9254a5759eea40109486836c
Attila Sovak <attila.sovak@gmail.com>
2025-05-02 14:45:41 +0200

    feat(argo-workflows): extend migrations with helm and manifest

--- migrations.tf
+++ migrations.tf
@@ -17,6 +17,17 @@ moved {
   from = aws_iam_role_policy_attachment.controller_additional["AllowArtifactsS3Bucket"]
   to   = module.addon-irsa["argo-workflows-controller"].aws_iam_role_policy_attachment.this_additional["AllowArtifactsS3Bucket"]
 }
+
+moved {
+  from = kubernetes_manifest.this[0]
+  to   = module.addon.kubernetes_manifest.this[0]
+}
+
+moved {
+  from = helm_release.this[0]
+  to   = module.addon.helm_release.this[0]
+}
+
 moved {
   from = helm_release.argo_application[0]
   to   = module.addon.helm_release.argo_application[0]
